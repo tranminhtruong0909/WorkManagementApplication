8e3022326ad3dc760f3fe0d468aa9f1d2908ba8f
Minh Truong <hihidongoc39@gmail.com>
2025-07-28 13:38:55 +0700

    Remove be folder from fe branch

--- be/workmanager/workmanager/src/main/java/com/example/workmanager/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.workmanager.repository;
-
-import com.example.workmanager.model.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Integer> {
-}
